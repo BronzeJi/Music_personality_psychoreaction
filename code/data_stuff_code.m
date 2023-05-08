10bdcb75b87738c19276cd975ab3d49fbf0a5e68
BronzeJi <jichenya@outlook.com>
2023-05-08 14:03:52 +0200

    code

---
+++ code/data_stuff_code.m
@@ -0,0 +1,36 @@
+%clear
+clc
+
+%% Loading data
+load("importeddata.mat");
+
+%% Remove text emotions
+
+person.song.Rock = textRemover(Musicengineeringanswers(:,8:16));
+
+person.song.Metal = textRemover(Musicengineeringanswers(:,17:25));
+
+person.song.Techno = textRemover(Musicengineeringanswers(:,26:34));
+
+person.song.EDM = textRemover(Musicengineeringanswers(:,35:43));
+
+person.song.Hardstyle = textRemover(Musicengineeringanswers(:,44:52));
+
+person.song.Jazz = textRemover(Musicengineeringanswers(:,53:61));
+
+person.song.Classical = textRemover(Musicengineeringanswers(:,62:70));
+
+person.song.Pop = textRemover(Musicengineeringanswers(:,71:79));
+
+person.song.RnB = textRemover(Musicengineeringanswers(:,80:88));
+
+person.song.Film = textRemover(Musicengineeringanswers(:,89:97));
+
+person.song.Kpop = textRemover(Musicengineeringanswers(:,98:106));
+
+person.song.Indie = textRemover(Musicengineeringanswers(:,107:115));
+
+person.song.PlayMusic = yesNo_data_remover(Musicengineeringanswers(:,6));
+
+
+
